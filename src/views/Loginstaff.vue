e6b97e6155edbbe210ac38ee63103e416335b613
Hazman97 <105151296+Hazman97@users.noreply.github.com>
2023-12-18 11:15:47 +0800

    siap log in page

--- src/views/Loginstaff.vue
+++ src/views/Loginstaff.vue
@@ -1,47 +1,47 @@
 <template>
     <!-- component -->
     <div class="background-image  h-screen w-screen flex items-center">
-        <div class="h-max mx-auto flex flex-col items-center bg-black shadow-lg bg-opacity-50 rounded-xl p-5">
+        <div class="h-max mx-auto flex flex-col items-center bg-[#18273F] shadow-lg bg-opacity-90 rounded-xl p-5">
             <img class="h-[60px] w-[70px] mb-5" src="@/assets/images/pkt-blue-logo.jpg" alt="">
             <h1 class="text-slate-200 shadow-text-2xl text-xl font-bold text-center pb-10">Sign in to your account</h1>
             <div class=" p-10 flex flex-col gap-4 text-sm">
                 <div>
-                    <label class="text-white font-bold inline-block pb-2" for="email">Email</label>
+                    <label class="text-slate-300 font-bold inline-block pb-2" for="email">Email</label>
                     <input class="border border-gray-400 focus:outline-slate-400 rounded-md w-full shadow-sm px-5 py-2"
-                        type="email" name="email" placeholder="mehedi@jaman.com">
+                        type="email" name="email" placeholder="user@pktgroup.com">
                 </div>
                 <div>
-                    <label class="text-white font-bold inline-block pb-2" for="password">Password</label>
+                    <label class="text-slate-300 font-bold inline-block pb-2" for="password">Password</label>
                     <input class="border border-gray-400 focus:outline-slate-400 rounded-md w-full shadow-sm px-5 py-2"
                         type="password" name="password" placeholder="******">
                 </div>
                 <div class="flex">
                     <div class="w-1/2">
                         <input type="checkbox" name="remeberMe">
-                        <label class="text-white" for="remeberMe">Remeber me</label>
+                        <label class="text-slate-200" for="remeberMe">Remeber me</label>
                     </div>
                     <div class="w-1/2">
-                        <a class="font-bold text-blue-600" href="">Forgot password ?</a>
+                        <a class="font-bold text-sky-300" href="">Forgot password ?</a>
                     </div>
                 </div>
                 <div>
-                    <input class="bg-[#4F46E5] w-full py-2 rounded-md text-white font-bold cursor-pointer hover:bg-blue-700"
+                    <input class="bg-[#18273F] w-full py-2 rounded-md text-white font-bold cursor-pointer hover:bg-blue-700"
                         type="submit" value="Login">
                 </div>
                 <div>
-                    <p class="text-center">Or continue with</p>
+                    <p class="text-slate-200 text-center">Or continue with</p>
                 </div>
-                <div class="flex gap-4">
-                    <img class="w-5 h-5 justify-center mx-auto"
+                <div class="flex gap-2">
+                    <img class=" w-8 h-5"
                         src="https://upload.wikimedia.org/wikipedia/commons/thumb/c/c9/Microsoft_Office_Teams_%282018%E2%80%93present%29.svg/1200px-Microsoft_Office_Teams_%282018%E2%80%93present%29.svg.png"
                         alt="">
                     <button
-                        class="bg-[#8087EE] w-full py-1 rounded-md text-white font-bold cursor-pointer hover:bg-blue-900">Microsoft
+                        class="bg-[#18273F] w-full py-1 rounded-md text-white font-bold cursor-pointer hover:text-sky-400">Microsoft
                         Teams</button>
                 </div>
             </div>
             <p class="text-sm text-slate-200 mt-10">If you vendor? <router-link to="/Vendorlogin"
-                    class="text-slate-100 font-bold">Login Here
+                    class="text-sky-400 font-bold">Login Here
                 </router-link></p>
         </div>
     </div>
@@ -68,11 +68,12 @@ export default {
     height: 100%;
     background-image: url('https://pktgroup.com/wp-content/uploads/2021/04/OAH-Thumbnail-4.jpg');
     background-size: cover;
-    filter: blur(5px);
+    filter: blur(2px);
     z-index: -1;
 }
 
 /* Additional styling or overrides for content if needed */
 .h-max {
     /* Add your styles for the content container */
-}</style>
+}
+</style>
