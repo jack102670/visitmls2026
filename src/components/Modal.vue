c03f5ec6a3d445cf8a29a2ff553cb3c23867ef14
Hazman97 <105151296+Hazman97@users.noreply.github.com>
2023-12-21 14:31:49 +0800

    incident form

--- src/components/Modal.vue
+++ src/components/Modal.vue
@@ -1,39 +1,51 @@
-<!-- components/ModalComponent.vue -->
-
 <template>
-    <transition name="fade">
-      <div v-if="modalOpen" class="main-modal fixed w-auto h-100 inset-0 z-50 overflow-hidden flex justify-center items-center animated fadeIn faster"
-        style="background: rgba(0,0,0,.7);">
-        <div class="border border-teal-500 shadow-lg modal-container bg-white w-6/12  mx-auto rounded shadow-lg z-50 overflow-y-auto">
-          <div class="modal-content py-4 text-left px-6">
-            <!--Title-->
-            <div class="flex justify-between items-center pb-3">
-              <p class="text-2xl font-bold">Confirmation</p>
-              <div class="modal-close cursor-pointer z-50" @click="closeModal">
-                <svg class="fill-current text-black" xmlns="http://www.w3.org/2000/svg" width="18" height="18"
-                  viewBox="0 0 18 18">
-                  <path
-                    d="M14.53 4.53l-1.06-1.06L9 7.94 4.53 3.47 3.47 4.53 7.94 9l-4.47 4.47 1.06 1.06L9 10.06l4.47 4.47 1.06-1.06L10.06 9z">
-                  </path>
-                </svg>
-              </div>
+  <transition name="fade">
+    <div v-if="modalOpen" class="main-modal fixed w-full h-100 inset-0 z-50 overflow-hidden flex justify-center items-center animated fadeIn faster"
+      style="background: rgba(0,0,0,.7);">
+      <div class="border border-teal-500 shadow-lg modal-container bg-white min-w-1/4 max-w-3xl min-w-700 min-h-1/4  mx-auto rounded shadow-lg z-50 overflow-y-auto">
+        <div class="modal-content py-4 text-left px-6">
+          <!-- Title -->
+          <div class="flex justify-between items-center pb-3">
+            <p class="text-2xl font-bold">Confirmation</p>
+            <div class="modal-close cursor-pointer z-50" @click="closeModal">
+              <svg class="fill-current text-black" xmlns="http://www.w3.org/2000/svg" width="18" height="18"
+                viewBox="0 0 18 18">
+                <path
+                  d="M14.53 4.53l-1.06-1.06L9 7.94 4.53 3.47 3.47 4.53 7.94 9l-4.47 4.47 1.06 1.06L9 10.06l4.47 4.47 1.06-1.06L10.06 9z">
+                </path>
+              </svg>
             </div>
-            <!--Body-->
-            <div class="my-5">
-              <slot></slot>
-            </div>
-            <!--Footer-->
-            <div class="flex justify-end pt-2">
-              <button @click="closeModal"
-                class="focus:outline-none modal-close px-4  text-sm font-medium tracking-wide text-gray-700 capitalize transition-colors duration-300 transform border border-gray-200 rounded-md sm:w-1/1 sm:mx-2 dark:text-gray-200 dark:border-gray-700 dark:hover:bg-gray-800 hover:bg-gray-100 focus:outline-none focus:ring focus:ring-gray-300 focus:ring-opacity-40">Cancel</button>
-              <button @click="closeModal"
-                class="focus:outline-none px-4 bg-teal-500 p-3 ml-3 rounded-lg px-4 py-2 mt-3 text-sm font-medium tracking-wide text-white capitalize transition-colors duration-300 transform bg-blue-600 rounded-md sm:mt-0 sm:w-1/1 sm:mx-2 hover:bg-blue-500 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-40">Confirm</button>
-            </div>
+          </div>
+          <!-- Body -->
+          <div class="my-5">
+            <slot></slot>
+          </div>
+          <!-- Footer -->
+          <div class="flex justify-end pt-2">
+            <button @click="closeModal"
+              class="focus:outline-none modal-close px-4 text-sm font-medium tracking-wide text-gray-700 capitalize transition-colors duration-300 transform border border-gray-200 rounded-md sm:w-1/1 sm:mx-2 dark:text-gray-200 dark:border-gray-700 dark:hover:bg-gray-800 hover:bg-gray-100 focus:outline-none focus:ring focus:ring-gray-300 focus:ring-opacity-40">Cancel</button>
+            <button @click="closeModal"
+              class="focus:outline-none px-4 bg-teal-500 p-3 ml-3 rounded-lg px-4 py-2 mt-3 text-sm font-medium tracking-wide text-white capitalize transition-colors duration-300 transform bg-blue-600 rounded-md sm:mt-0 sm:w-1/1 sm:mx-2 hover:bg-blue-500 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-40">Confirm</button>
           </div>
         </div>
       </div>
-    </transition>
-  </template>
+    </div>
+  </transition>
+</template>
+
+<style scoped>
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.5s;
+}
+.fade-enter,
+.fade-leave-to {
+  opacity: 0;
+}
+</style>
+
+
+
   
   <script>
   export default {
@@ -61,4 +73,3 @@
     opacity: 0;
   }
   </style>
-  
--- src/views/Incidentreportform.vue
+++ src/views/Incidentreportform.vue
@@ -7,7 +7,7 @@
         class="bg-[#f7fbff] dark:bg-gray-800 dark:ring-offset-gray-900 border-gray-200 dark:border-gray-700 rounded-lg px-6 py-8 ring-1 ring-slate-900/5 shadow-xl"
       >
         <section
-          class="max-w-4xl p-6 mx-auto bg-white rounded-md shadow-md dark:bg-gray-800"
+          class="max-w-4xl p-6 mx-auto bg-[#f7fbff] rounded-md dark:bg-gray-800"
         >
           <h2
             class="text-lg font-semibold text-gray-700 capitalize dark:text-white"
@@ -15,15 +15,20 @@
             Incident Report Form
           </h2>
 
-          <form>
-            <div class="grid grid-cols-1 gap-6 mt-4 sm:grid-cols-2">
+          <form @submit.prevent="openPreviewModal">
+            <div
+              class="grid grid-cols-1 gap-6 mt-4 sm:grid-cols-2 md:grid-cols-3"
+            >
               <div>
-                <label class="text-gray-700 dark:text-gray-200" for="username"
-                  >Requester Name</label
+                <label
+                  class="text-gray-700 dark:text-gray-200"
+                  for="Dateofincident"
+                  >Date of incident</label
                 >
                 <input
-                  id="username"
-                  type="text"
+                  v-model="formData.Dateofincident"
+                  id="Dateofincident"
+                  type="Date"
                   class="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring"
                 />
               </div>
@@ -31,45 +36,55 @@
               <div>
                 <label
                   class="text-gray-700 dark:text-gray-200"
-                  for="emailAddress"
-                  >Department</label
+                  for="Timeofincident"
+                  >Time of incident</label
                 >
                 <input
-                  id="emailAddress"
-                  type="email"
-                  class="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring"
-                />
-              </div>
-
-              <div>
-                <label class="text-gray-700 dark:text-gray-200" for="password"
-                  >Incident Date</label
-                >
-                <input
-                  id="incidentdate"
-                  type="date"
+                  v-model="formData.Timeofincident"
+                  id="Timeofincident"
+                  type="time"
                   class="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring"
                 />
               </div>
               <div>
-                <label class="text-gray-700 dark:text-gray-200" for="password"
-                  >Incident time</label
+                <label
+                  class="text-gray-700 dark:text-gray-200"
+                  for="Locationofincident"
+                  >Location of incident</label
                 >
                 <input
-                  id="incidenttime"
+                  v-model="formData.Locationofincident"
+                  id="Locationofincident"
                   type="time"
                   class="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring"
                 />
               </div>
 
+              <!-- Add v-model to other inputs as needed -->
+            </div>
+
+            <div class="grid grid-cols-1 gap-6 mt-4 sm:grid-cols-3">
               <div>
                 <label
                   class="text-gray-700 dark:text-gray-200"
-                  for="passwordConfirmation"
-                  >Incident Location</label
+                  for="PartiesInvolved"
+                  >Name of parties involved</label
                 >
                 <input
-                  id="passwordConfirmation"
+                  v-model="formData.PartiesInvolved"
+                  id="PartiesInvolved"
+                  type="text"
+                  class="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring"
+                />
+              </div>
+
+              <div>
+                <label class="text-gray-700 dark:text-gray-200" for="Witness"
+                  >Witness/victim/suspect</label
+                >
+                <input
+                  v-model="formData.Witness"
+                  id="Witness"
                   type="text"
                   class="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring"
                 />
@@ -77,69 +92,285 @@
               <div>
                 <label
                   class="text-gray-700 dark:text-gray-200"
-                  for="passwordConfirmation"
-                  >Incident Location</label
+                  for="Typeofincident"
+                  >Type of incident</label
                 >
                 <input
-                  id="passwordConfirmation"
+                  v-model="formData.Typeofincident"
+                  id="Typeofincident"
                   type="text"
                   class="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring"
                 />
               </div>
-              <div>
+
+              <!-- Add v-model to other inputs as needed -->
+            </div>
+            <div
+              class="grid grid-cols-1 gap-6 mt-4 sm:grid-cols-1 mx-auto cursor-pointer flex w-full"
+            >
+              <div class="w-full">
                 <label
+                  for="Detailsincident"
                   class="text-gray-700 dark:text-gray-200"
-                  for="passwordConfirmation"
-                  >Witness</label
+                  >Details Incident</label
                 >
-                <input
-                  id="passwordConfirmation"
-                  type="text"
-                  class="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring"
-                />
+                <textarea
+                  v-model="formData.Detailsincident"
+                  id="Detailsincident"
+                  class="w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring"
+                  rows="5"
+                  required
+                ></textarea>
               </div>
+            </div>
+
+            <div class="grid grid-cols-1 gap-6 mt-4 sm:grid-cols-1">
               <div>
-                <label
-                  class="text-gray-700 dark:text-gray-200"
-                  for="passwordConfirmation"
-                  >Details</label
-                >
-                <input
-                  id="passwordConfirmation"
-                  type="text"
-                  class="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring"
-                />
+                <label class="text-gray-700 dark:text-gray-200" for="username"
+                  >Attach file (if necessary)
+                </label>
+
+                <!-- component -->
+
+                <div>
+                  <label
+                    for="dropzone-file"
+                    class="p-10 flex mx-auto cursor-pointer flex w-full max-w-lg flex-col items-center rounded-xl border-2 border-dashed"
+                    :class="{
+                      'border-blue-400': !isDragging,
+                      'border-blue-700': isDragging,
+                      'bg-white': !isDragging,
+                      'bg-blue-100': isDragging,
+                    }"
+                    @dragover.prevent="handleDragOver"
+                    @dragenter.prevent="handleDragEnter"
+                    @dragleave.prevent="handleDragLeave"
+                    @drop.prevent="handleDrop"
+                  >
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      class="h-10 w-10 text-blue-500"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      stroke="currentColor"
+                      stroke-width="2"
+                    >
+                      <path
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        d="M7 16a4 4 0 01-.88-7.903A5 5 0 1115.9 6L16 6a5 5 0 011 9.9M15 13l-3-3m0 0l-3 3m3-3v12"
+                      />
+                    </svg>
+
+                    <h2
+                      class="mt-4 text-xl font-medium text-gray-700 tracking-wide"
+                    >
+                      Click Here
+                    </h2>
+
+                    <p class="mt-2 text-gray-500 tracking-wide">
+                      Upload or drag & drop your file SVG, PNG, JPG, or GIF.
+                    </p>
+
+                    <input
+                      id="dropzone-file"
+                      type="file"
+                      class="hidden"
+                      @change="handleFileChange"
+                    />
+                  </label>
+
+                  <!-- Display uploaded files -->
+                  <ul class="mt-4">
+                    <li
+                      v-for="(file, index) in uploadedFiles"
+                      :key="index"
+                      class="flex items-center justify-between bg-blue-100 p-4 mb-2 rounded-md"
+                    >
+                      <span class="text-blue-700">{{ file.name }}</span>
+                      <button
+                        class="bg-blue-900 text-white px-4 py-2 rounded-full hover:bg-blue-700 focus:outline-none focus:shadow-outline"
+                        @click="removeFile(index)"
+                      >
+                        Remove
+                      </button>
+                    </li>
+                  </ul>
+
+                  <!-- Display uploaded files -->
+                </div>
               </div>
-              <div>
-                <label
-                  class="text-gray-700 dark:text-gray-200"
-                  for="passwordConfirmation"
-                  >appointment time</label
-                >
-                <input
-                  id="passwordConfirmation"
-                  type="text"
-                  class="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring"
-                />
-              </div>
+
+              <!-- Add v-model to other inputs as needed -->
             </div>
 
             <div class="flex justify-end mt-6">
               <button
+                type="submit"
                 class="px-8 py-2.5 leading-5 text-white transition-colors duration-300 transform bg-gray-700 rounded-md hover:bg-gray-600 focus:outline-none focus:bg-gray-600"
               >
-                submit
+                Submit
               </button>
             </div>
           </form>
         </section>
       </div>
     </div>
+
+    <Modal :modalOpen="isPreviewModalOpen" :closeModal="closePreviewModal">
+      <!-- Content for your custom modal -->
+      <div class="bg-white dark:bg-gray-800 rounded-lg p-8 shadow-lg">
+        <h3 class="text-2xl font-bold text-gray-700 dark:text-white mb-4">
+          Preview
+        </h3>
+
+        <table
+          class="w-full mt-4 bg-white border border-gray-300 divide-y divide-gray-300"
+        >
+          <tr>
+            <td class="py-2 px-4 font-medium">Requester Name:</td>
+            <td class="py-2 px-4">{{ formData.Dateofincident }}</td>
+          </tr>
+          <tr>
+            <td class="py-2 px-4 font-medium">Department:</td>
+            <td class="py-2 px-4">{{ formData.Timeofincident }}</td>
+          </tr>
+          <tr>
+            <td class="py-2 px-4 font-medium">Phone Number:</td>
+            <td class="py-2 px-4">{{ formData.Locationofincident }}</td>
+          </tr>
+          <tr>
+            <td class="py-2 px-4 font-medium">Date Requested:</td>
+            <td class="py-2 px-4">{{ formData.PartiesInvolved }}</td>
+          </tr>
+          <tr>
+            <td class="py-2 px-4 font-medium">People/For:</td>
+            <td class="py-2 px-4">{{ formData.Typeofincident }}</td>
+          </tr>
+          <tr>
+            <td class="py-2 px-4 font-medium">Details:</td>
+            <td class="py-2 px-4">
+              <div class="text-sm max-h-32 overflow-auto">
+                {{ formData.Detailsincident }}
+              </div>
+            </td>
+          </tr>
+
+          <!-- ... (Other modal content) ... -->
+
+          <tr>
+            <td class="py-2 px-4 font-medium">Uploaded Files:</td>
+            <td class="py-2 px-4">
+              <ul>
+                <li
+                  v-for="(file, index) in uploadedFiles"
+                  :key="index"
+                  class="flex items-center bg-blue-100 p-4 mb-2 rounded-md"
+                >
+                  <span class="text-blue-700">{{ file.name }}</span>
+                </li>
+              </ul>
+            </td>
+          </tr>
+
+          <!-- ... (Other modal content) ... -->
+        </table>
+
+        <!-- <div class="flex justify-end mt-6">
+          <button
+            type="submit"
+            class="px-8 py-2.5 leading-5 text-white transition-colors duration-300 transform bg-gray-700 rounded-md hover:bg-gray-600 focus:outline-none focus:bg-gray-600"
+          >
+            Submit
+          </button>
+        </div> -->
+      </div>
+    </Modal>
+
+    <!-- <div class="flex justify-end mt-6">
+          <button
+            type="submit"
+            class="px-6 py-2 bg-blue-500 text-white rounded-md hover:bg-blue-600 focus:outline-none"
+          >
+            Submit
+          </button>
+        </div> -->
   </main>
 </template>
 
 <script>
+import Modal from "@/components/Modal.vue";
+
 export default {
   name: "cctvfromViews",
+  components: {
+    Modal,
+  },
+  data() {
+    return {
+      uploadedFiles: [],
+      isDragging: false,
+      formData: {
+        requesterName: "",
+        department: "",
+        phonenumber: "",
+        daterequest: "",
+        location: "",
+        stockinbox: "",
+        stockinkit: "",
+        kitout: "",
+        balancekit: "",
+        // Add more form fields here
+      },
+      isPreviewModalOpen: false,
+    };
+  },
+  computed: {
+    labelClasses() {
+      return {
+        "border-blue-400": !this.isDragging,
+        "border-blue-700": this.isDragging,
+        "bg-white": !this.isDragging,
+        "bg-blue-100": this.isDragging,
+      };
+    },
+  },
+  methods: {
+    openPreviewModal() {
+      this.isPreviewModalOpen = true;
+    },
+    closePreviewModal() {
+      this.isPreviewModalOpen = false;
+    },
+    handleDragOver() {
+      this.isDragging = true;
+    },
+    handleDragEnter() {
+      this.isDragging = true;
+    },
+    handleDragLeave() {
+      this.isDragging = false;
+    },
+    handleDrop(event) {
+      this.isDragging = false;
+      // Handle the dropped files
+      const files = event.dataTransfer.files;
+      this.handleFiles(files);
+    },
+    handleFileChange(event) {
+      // Handle the selected files
+      const files = event.target.files;
+      this.handleFiles(files);
+    },
+    handleFiles(files) {
+      // Process and store the files
+      for (const file of files) {
+        this.uploadedFiles.push(file);
+      }
+    },
+    removeFile(index) {
+      this.uploadedFiles.splice(index, 1);
+    },
+  },
 };
 </script>
