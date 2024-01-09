55431b0cb69fb620290e5131d07bbd668bcfba9d
Hazman97 <105151296+Hazman97@users.noreply.github.com>
2024-01-09 17:23:56 +0800

    sved

--- src/views/cctvform.vue
+++ src/views/cctvform.vue
@@ -75,19 +75,7 @@
             </div>
 
             <div class="grid grid-cols-1 gap-6 mt-4 sm:grid-cols-3">
-              <div>
-                <label
-                  class="font-semibold text-gray-700 dark:text-gray-200"
-                  for="Daterequested"
-                  >Date Requested</label
-                >
-                <input
-                  v-model="formData.Daterequested"
-                  id="Daterequested"
-                  type="date"
-                  class="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring"
-                />
-              </div>
+          
 
               <div>
                 <label
@@ -208,10 +196,7 @@
             <td class="py-2 px-4 font-medium">Phone Number:</td>
             <td class="py-2 px-4">{{ formData.Phonenumber }}</td>
           </tr>
-          <tr>
-            <td class="py-2 px-4 font-medium">Date Requested:</td>
-            <td class="py-2 px-4">{{ formData.Daterequested }}</td>
-          </tr>
+     
           <tr>
             <td class="py-2 px-4 font-medium">Incident Location:</td>
             <td class="py-2 px-4">{{ formData.Incidentlocation }}</td>
