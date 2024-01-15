b17cdce248243909aac9768d605b28ee0649d88b
Hazman97 <105151296+Hazman97@users.noreply.github.com>
2024-01-15 17:39:03 +0800

    .

--- src/views/badgeform.vue
+++ src/views/badgeform.vue
@@ -35,10 +35,10 @@
                   class="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring"
                 />
                 <input
-                  v-model="branch"
+                  v-model="FormData.branch"
                   id="requesterName"
                   type="hidden"
-                  required
+                  
                   class="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring"
                 />
               </div>
@@ -281,7 +281,7 @@ export default {
   },
   data() {
     return {
-      selectedLocation: formData.branch,
+
       isLoading: false,
       myFiles: [],
       uploadedFileNames: [],
