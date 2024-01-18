a7dd3fa3c275d50bc99b7569cef8f4de19dcb43d
hazman5540 <86184272+hazman5540@users.noreply.github.com>
2024-01-18 23:08:56 +0800

    .,

--- src/views/badgeform.vue
+++ src/views/badgeform.vue
@@ -5,7 +5,7 @@
         class="bg-[#f7fbff] dark:bg-gray-800 dark:ring-offset-gray-900 border-gray-200 dark:border-gray-700 rounded-lg px-6 py-8 ring-1 ring-slate-900/5 shadow-xl">
         <section class="max-w-4xl p-6 mx-auto bg-white border-2 border-e-gray-200 rounded-md dark:bg-gray-800">
           <h2 class="text-2xl font-bold text-gray-700 capitalize dark:text-white">
-            BADGE REQUEST FORM {{ branch }}
+            BADGE REQUEST FORM {{ userDetails.userId }}
           </h2>
           <div class="pt-2">
             <hr class="" />
@@ -331,7 +331,9 @@ export default {
       });
       // Construct the URL with userId and uniqueCode
       const url = `http://172.28.28.91:8085/api/Files/MultiUploadImage/${this.userDetails.userId}/${this.uniqueCode}`;
-      axios.post(url, formData)
+       console.log('userid:', this.userDetails.userId); 
+       axios.post(url, formData)
+    
         .then(response => {
           console.log('Upload successful:', response.data);
         })
