f9ccec96ace9754f7756fdd162de48b5b525481e
ernaliazman <86944098+ernaliazman@users.noreply.github.com>
2024-01-22 10:42:19 +0800

    update loading
    update loading spinner

--- src/components/Tablemanagerequest.vue
+++ src/components/Tablemanagerequest.vue
@@ -1064,13 +1064,22 @@
 
       <!-- Update button -->
       <div class="flex justify-end mt-6">
-        <button
-          @click="adminUpdate(getRequest.referenceNumber)"
-          type="button"
-          class="px-8 py-2.5 leading-5 text-white transition-colors duration-300 transform bg-sky-700 rounded-md hover:bg-gray-600 focus:outline-none focus:bg-gray-600"
-        >
-          Update
-        </button>
+        <div v-show= "showConfirmButton">
+          <button @click= adminUpdate() type="button" class="text-center rounded-2xl bg-sky-800 shadow-md p-3 my-1 w-full text-white py-2.5 px-5 mr-2 text-sm font-medium text-gray-900 rounded border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:outline-none focus:ring-blue-700 focus:text-blue-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700  items-center">
+   
+    Update
+</button>
+</div>
+<div v-show = "showLoadingButton">
+  <button disabled="" type="button" class="text-center rounded-2xl bg-sky-800 shadow-md p-3 my-1 w-full text-white py-2.5 px-5 mr-2 text-sm font-medium text-gray-900 rounded border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:outline-none focus:ring-blue-700 focus:text-blue-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700  items-center">
+    <svg aria-hidden="true" role="status" class="inline mr-2 w-4 h-4 text-gray-200 animate-spin dark:text-gray-600" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="currentColor"></path>
+    <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="#1C64F2"></path>
+    </svg>
+    Loading....
+</button>
+</div>
+
       </div>
     </template>
   </Modal>
@@ -1086,6 +1095,9 @@ export default {
   },
   data() {
     return {
+      showConfirmButton: true,
+      showLoadingButton: false,
+
       getRequest: [],
       isModalVisible: false,
       showBadgeRequest: false,
@@ -1115,6 +1127,8 @@ export default {
       return parts[parts.length - 1];
     },
     adminUpdate() {
+      this.showConfirmButton = false;
+      this.showLoadingButton= true;
       if (this.getRequest.referenceNumber.includes("BR")) {
         axios
           .put("http://172.28.28.91:8085/api/Admin/AdminUpdateBadgeRequest", {
@@ -1125,12 +1139,18 @@ export default {
             modifiedBy: this.getRequest.modifiedBy,
             userEmail: "hazman5001@gmail.com",
           })
+          .then((response) => {
+            "Server response:", response.data;
+            // Handle the response, such as showing a success message or resetting the form
+            //myrequest.fetchRequesters();
+
+            this.isModalVisible = false;
+          window.location.reload();
+          })
           .catch((error) => {
             console.error("Error:", error);
           });
 
-        this.isModalVisible = false;
-        window.location.reload();
         //this.showBadgeRequest = true;
       }
       if (this.getRequest.referenceNumber.includes("IR")) {
@@ -1143,12 +1163,18 @@ export default {
             modifiedBy: this.getRequest.modifiedBy,
             userEmail: "hazman5001@gmail.com",
           })
+          .then((response) => {
+            "Server response:", response.data;
+            // Handle the response, such as showing a success message or resetting the form
+            //myrequest.fetchRequesters();
+
+            this.isModalVisible = false;
+          window.location.reload();
+          })
           .catch((error) => {
             console.error("Error:", error);
           });
 
-        this.isModalVisible = false;
-        window.location.reload();
         //this.showBadgeRequest = true;
       }
       if (this.getRequest.referenceNumber.includes("CCTV")) {
@@ -1161,12 +1187,18 @@ export default {
             modifiedBy: this.getRequest.modifiedBy,
             userEmail: "hazman5001@gmail.com",
           })
+          .then((response) => {
+            "Server response:", response.data;
+            // Handle the response, such as showing a success message or resetting the form
+            //myrequest.fetchRequesters();
+
+            this.isModalVisible = false;
+            window.location.reload();
+          })
           .catch((error) => {
             console.error("Error:", error);
           });
 
-        this.isModalVisible = false;
-        window.location.reload();
         //this.showBadgeRequest = true;
       }
       if (this.getRequest.referenceNumber.includes("Mask")) {
@@ -1179,12 +1211,18 @@ export default {
             modifiedBy: this.getRequest.modifiedBy,
             userEmail: "hazman5001@gmail.com",
           })
+          .then((response) => {
+            "Server response:", response.data;
+            // Handle the response, such as showing a success message or resetting the form
+            //myrequest.fetchRequesters();
+
+            this.isModalVisible = false;
+          window.location.reload();
+          })
           .catch((error) => {
             console.error("Error:", error);
           });
 
-        this.isModalVisible = false;
-        window.location.reload();
         //this.showBadgeRequest = true;
       }
       if (this.getRequest.referenceNumber.includes("TK")) {
@@ -1197,12 +1235,19 @@ export default {
             modifiedBy: this.getRequest.modifiedBy,
             userEmail: "hazman5001@gmail.com",
           })
+          .then((response) => {
+            "Server response:", response.data;
+            // Handle the response, such as showing a success message or resetting the form
+            //myrequest.fetchRequesters();
+
+            this.isModalVisible = false;
+          window.location.reload();
+          })
           .catch((error) => {
             console.error("Error:", error);
           });
 
-        this.isModalVisible = false;
-        window.location.reload();
+        
         //this.showBadgeRequest = true;
       }
       if (this.getRequest.referenceNumber.includes("VET")) {
@@ -1215,12 +1260,18 @@ export default {
             modifiedBy: this.getRequest.modifiedBy,
             userEmail: "hazman5001@gmail.com",
           })
+          .then((response) => {
+            "Server response:", response.data;
+            // Handle the response, such as showing a success message or resetting the form
+            //myrequest.fetchRequesters();
+
+            this.isModalVisible = false;
+          window.location.reload();
+          })
           .catch((error) => {
             console.error("Error:", error);
           });
 
-        this.isModalVisible = false;
-        window.location.reload();
         //this.showBadgeRequest = true;
       }
       if (this.getRequest.referenceNumber.includes("PTW")) {
