54a4fed45cade2ba51a17c91426ddbcb54b040c7
Hazman97 <105151296+Hazman97@users.noreply.github.com>
2024-03-06 09:11:40 +0800

    Update file links in AccApproval.vue (generated by blackbox.ai )

--- src/views/AccApproval.vue
+++ src/views/AccApproval.vue
@@ -142,7 +142,32 @@
                           class="px-3 py-2 text-sm text-gray-500 dark:text-gray-300 whitespace-nowrap"
                         >
                           <div v-for="file in requester.files" :key="file">
-                            <a class="text-blue-500 block mb-1" :href="file" target="_blank">&#8226; {{ getFileName(file) }}</a>
+                            <div
+                              class="text-black-500 block mb-1 inline-flex items-center"
+                            >
+                              <a :href="file"  class="text-blue-500" target="_blank">
+                                <svg
+                                  xmlns="http://www.w3.org/2000/svg"
+                                  fill="none"
+                                  viewBox="0 0 24 24"
+                                  stroke-width="1.5"
+                                  stroke="currentColor"
+                                  class="w-5 h-5 inline-block align-middle"
+                                >
+                                  <path
+                                    stroke-linecap="round"
+                                    stroke-linejoin="round"
+                                    d="M2.036 12.322a1.012 1.012 0 010-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178z"
+                                  />
+                                  <path
+                                    stroke-linecap="round"
+                                    stroke-linejoin="round"
+                                    d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"
+                                  />
+                                </svg>
+                              </a>
+                              <span class="ml-1">{{ getFileName(file) }}</span>
+                            </div>
 
                             <br />
                             <!-- Add a line break after each file link -->
