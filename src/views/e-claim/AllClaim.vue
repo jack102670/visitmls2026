4f5d92b935055ebda732ff4804636d347783cd29
FatinRashid <fatinursyafawani@gmail.com>
2024-04-23 09:31:30 +0800

    UPDATED

---
+++ src/views/e-claim/AllClaim.vue
@@ -0,0 +1,26 @@
+<template>
+  <main class="flex-1 overflow-x-hidden text overflow-y-auto bg-[#CED1DA] dark:bg-[#111827] p-4 sm:ml-64 h-auto">
+    <div class="container mx-auto">
+      <div class="bg-[#f7fbff] dark:bg-gray-800 dark:ring-offset-gray-900 border-gray-200 dark:border-gray-700 rounded-lg px-6 py-8 ring-1 ring-slate-900/5 shadow-xl">
+        <div class="flex items-center justify-between mb-6">
+          <h2 class="text-xl font-bold text-slate-900 dark:text-white"></h2>
+        </div>
+
+        <section class="container px-4 mx-auto">
+          <div class="flex items-center justify-between gap-x-3">
+            <div>
+              <h2 class="text-lg font-medium text-gray-800 dark:text-white">My Request</h2>
+            </div>
+          </div>
+        </section>
+      </div>
+    </div>
+  </main>
+</template>
+
+<script>
+
+</script>
+<style>
+
+</style>
