07c1b7256d948d55b2fed529dc140a375b37aedb
Hazman97 <105151296+Hazman97@users.noreply.github.com>
2024-04-24 10:44:48 +0800

    Update CSS classes and structure in Testing.vue
    - Replaced P tag with p tag for lorem text
    - Updated grid structure and added margin left
    - Changed background color and added hover effect for white boxes
    - Add

--- src/views/Testing.vue
+++ src/views/Testing.vue
@@ -1,29 +1,39 @@
 <template>
   <div class="bg-pink-700 h-screen">
-    <P class="text-4xl p-4">lorem</P>
-    <div class="grid grid-cols-1 md:grid-cols-2"> 
-     
-      <div class="bg-white rounded-lg p-10">
-        <div
-          class="grid grid-cols-1 md:grid-cols-4 sm:grid-cols-3 m-3 rounded-md gap-4 p-2"
-        >
-          <div class="min-h-[100px] rounded bg-red-500"></div>
-          <div class="min-h-[100px] rounded bg-yellow-500"></div>
-          <div class="min-h-[100px] rounded bg-green-500"></div>
-          <div class="min-h-[100px] rounded bg-blue-500"></div>
+    <p class="text-4xl p-4">lorem</p>
+    <div class="grid grid-cols-1 md:grid-cols-2 ml-3"> 
+      <div class="bg-white/30 backdrop-blur-sm rounded-lg p-5">
+        <h1 class="text-center text-3xl text-slate-100">PKT LOGISTICS</h1>
+        <div class="grid grid-cols-1 md:grid-cols-4 sm:grid-cols-3 m-3 rounded-md gap-4 p-2">
+          <div class="min-h-[100px] rounded bg-white hover:bg-slate-200">
+            <p class="font-semibold text-center">Security</p>
+            <hr>
+          </div>
+          <div class="min-h-[100px] rounded bg-white hover:bg-slate-200">
+            <p class="font-semibold text-center">E-Claim</p>
+            <hr>
+            <img src="https://img.icons8.com/external-others-phat-plus/64/external-claim-shipping-color-line-others-phat-plus.png" alt="e-claim" class="w-10 h-10 mx-auto p-1">
+          </div>
+          <div class="min-h-[100px] rounded bg-white hover:bg-slate-200">
+            <p class="font-semibold text-center">Safety</p>
+            <hr>
+            <h1></h1>
+          </div>    <div class="min-h-[100px] rounded bg-white overflow-hidden hover:bg-slate-200">
+            <p class="font-semibold text-center">Maintenance</p>
+            <hr>
+          </div>
         </div>
-        <div
-          class="grid grid-cols-1 md:grid-cols-4 sm:grid-cols-3 m-3 rounded-md gap-4 p-2"
-        >
-          <div class="min-h-[100px] rounded bg-red-500"></div>
-          <div class="min-h-[100px] rounded bg-yellow-500"></div>
-          <div class="min-h-[100px] rounded bg-green-500"></div>
-          <div class="min-h-[100px] rounded bg-blue-500"></div>
+        <div class="grid grid-cols-1 md:grid-cols-4 sm:grid-cols-3 m-3 rounded-md gap-4 p-2">
+          <div class="min-h-[100px] rounded bg-white-500"></div>
+          <div class="min-h-[100px] rounded bg-white-500"></div>
+          <div class="min-h-[100px] rounded bg-white-500"></div>
+          <div class="min-h-[100px] rounded bg-white-500"></div>
         </div>
       </div>
     </div>
   </div>
 </template>
+
 
 <script>
 export default {
