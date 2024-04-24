3b7382fdcc0e0fe44077bc03b93c708504047ab0
FatinRashid <fatinursyafawani@gmail.com>
2024-04-24 08:44:41 +0800

    update boxinfo

--- src/components/e-claim/BoxInfo.vue
+++ src/components/e-claim/BoxInfo.vue
@@ -41,92 +41,82 @@
               </div>
             </button>
           </div>
-        </div>
+         <div>
+        <h2 class="text-lg font-semibold text-gray-800 dark:text-white">
+          Status
+        </h2>
       </div>
-    </div>
-  </main>
-  <main class="flex-1 text overflow-y-auto bg-[#CED1DA] p-4 sm:ml-64">
-    <div class="container mx-auto">
       <div
-        class="relative overflow-hidden bg-[#f7fbff] border-gray-200 rounded-lg px-6 py-8 ring-1 ring-slate-900/5 shadow-xl"
+        class="mt-5 grid grid-cols-2 gap-3 sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-4"
       >
-        <div>
-          <h2 class="text-lg font-semibold text-gray-800 dark:text-white">
-            Status
-          </h2>
+        <div
+          class="relative overflow-hidden bg-white shadow-md border-x-2 border-y-2 border-blue-700 rounded-lg"
+        >
+          <div class="card m-2">
+            <div class="m-2">
+              <span class="text-2xl font-bold">00</span>
+              <span class="block text-sm text-gray-500 font-semibold"
+                >Draft</span
+              >
+            </div>
+            <div
+              class="flex items-center justify-center w-full top-0 mt-2 bg-blue-700 p-2 text-white text-xs font-bold"
+            >
+              <span>See More</span>
+            </div>
+          </div>
         </div>
         <div
-          class="mt-5 grid grid-cols-2 gap-1 sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-4"
+          class="relative overflow-hidden bg-white shadow-md border-x-2 border-y-2 border-blue-700 rounded-lg"
         >
-          <div
-            class="relative overflow-hidden bg-white shadow-md border-x-2 border-y-2 border-blue-700 rounded-lg"
-          >
-            <div class="card">
-              <div class="m-4">
-                <span class="text-2xl font-bold">00</span>
-                <span class="block text-sm text-gray-500 font-semibold"
-                  >Draft</span
-                >
-              </div>
-              <div
-                class="flex items-center justify-center w-full top-0 mt-2 bg-blue-700 p-2 text-white text-xs font-bold"
+          <div class="card m-2">
+            <div class="m-2">
+              <span class="text-2xl font-bold">00</span>
+              <span class="block text-sm text-gray-500 font-semibold"
+                >Unsubmitted</span
               >
-                <span>See More</span>
-              </div>
+            </div>
+            <div
+              class="flex items-center justify-center w-full top-0 mt-2 bg-blue-700 p-2 text-white text-xs font-bold"
+            >
+              <span>See More</span>
             </div>
           </div>
-          <div
-            class="relative overflow-hidden bg-white shadow-md border-x-2 border-y-2 border-blue-700 rounded-lg"
-          >
-            <div class="card">
-              <div class="m-4">
-                <span class="text-2xl font-bold">00</span>
-                <span class="block text-sm text-gray-500 font-semibold"
-                  >Unsubmitted</span
-                >
-              </div>
-              <div
-                class="flex items-center justify-center w-full top-0 mt-2 bg-blue-700 p-2 text-white text-xs font-bold"
+        </div>
+        <div
+          class="relative overflow-hidden bg-white shadow-md border-x-2 border-y-2 border-blue-700 rounded-lg"
+        >
+          <div class="card m-2">
+            <div class="m-2">
+              <span class="text-2xl font-bold">00</span>
+              <span class="block text-sm text-gray-500 font-semibold"
+                >Submitted</span
               >
-                <span>See More</span>
-              </div>
+            </div>
+            <div
+              class="flex items-center justify-center w-full top-0 mt-2 bg-blue-700 p-2 text-white text-xs font-bold"
+            >
+              <span>See More</span>
             </div>
           </div>
-          <div
-            class="relative overflow-hidden bg-white shadow-md border-x-2 border-y-2 border-blue-700 rounded-lg"
-          >
-            <div class="card">
-              <div class="m-4">
-                <span class="text-2xl font-bold">00</span>
-                <span class="block text-sm text-gray-500 font-semibold"
-                  >Submitted</span
-                >
-              </div>
-              <div
-                class="flex items-center justify-center w-full top-0 mt-2 bg-blue-700 p-2 text-white text-xs font-bold"
+        </div>
+        <div
+          class="relative overflow-hidden bg-white shadow-md border-x-2 border-y-2 border-blue-700 rounded-lg"
+        >
+          <div class="card m-2">
+            <div class="m-2">
+              <span class="text-2xl font-bold">00</span>
+              <span class="block text-sm text-gray-500 font-semibold"
+                >All Booking</span
               >
-                <span>See More</span>
-              </div>
+            </div>
+            <div
+              class="flex items-center justify-center w-full top-0 mt-2 bg-blue-700 p-2 text-white text-xs font-bold"
+            >
+              <span>See More</span>
             </div>
           </div>
-          <div
-            class="relative overflow-hidden bg-white shadow-md border-x-2 border-y-2 border-blue-700 rounded-lg"
-          >
-            <div class="card">
-              <div class="m-4">
-                <span class="text-2xl font-bold">00</span>
-                <span class="block text-sm text-gray-500 font-semibold"
-                  >All Booking</span
-                >
-              </div>
-              <div
-                class="flex items-center justify-center w-full top-0 mt-2 bg-blue-700 p-2 text-white text-xs font-bold"
-              >
-                <span>See More</span>
-              </div>
-            </div>
-          </div>
-          
+        </div>
         </div>
       </div>
     </div>
