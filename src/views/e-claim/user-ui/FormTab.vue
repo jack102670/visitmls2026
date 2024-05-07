3376d64e3550a77a03cb92fc66ad07557b8f0495
FatinRashid <fatinursyafawani@gmail.com>
2024-05-07 15:58:48 +0800

    update FormTab.vue

--- src/views/e-claim/user-ui/FormTab.vue
+++ src/views/e-claim/user-ui/FormTab.vue
@@ -1,73 +1,91 @@
 <template>
-        <div
-          class="relative overflow-hidden bg-[#f7fbff] dark:bg-gray-800 dark:ring-offset-gray-900 border-gray-200 dark:border-gray-700 rounded-lg px-6 py-8 ring-1 ring-slate-900/5 shadow-xl"
-        >
-          <div class="sm:flex justify-center flex-col-1">
-            <button
-              v-for="(tab, index) in tabs"
-              :key="index"
-              @click="activeTab = index"
-              :class="{
-                'bg-gray-300': activeTab === index,
-                'hover:bg-gray-200': activeTab !== index,
-              }"
-              class="px-4 py-2 mr-2 rounded-md focus:outline-none"
-            >
-              {{ tab.title }}
-            </button>
-          </div>
+  <div
+    class="relative overflow-hidden bg-[#f7fbff] dark:bg-gray-800 dark:ring-offset-gray-900 border-gray-200 dark:border-gray-700 rounded-lg px-6 py-8 ring-1 ring-slate-900/5 shadow-xl"
+  >
+    <div class="sm:flex justify-center flex-col-1">
+      <button
+        v-for="(tab, index) in tabs"
+        :key="index"
+        @click="activeTab = index"
+        :class="{
+          'bg-gray-300': activeTab === index,
+          'hover:bg-gray-200': activeTab !== index,
+        }"
+        class="px-4 py-2 mr-2 rounded-md focus:outline-none"
+      >
+        {{ tab.title }}
+      </button>
+    </div>
 
+    <div
+      class="relative overflow-hidden mt-5 max-w-4xl p-6 mx-auto bg-white border-2 border-e-gray-200 rounded-md dark:bg-gray-800"
+    >
+      <div
+        v-for="(tab, index) in tabs"
+        :key="index"
+        v-show="activeTab === index"
+      >
+        <h2
+          class="text-2xl font-bold text-gray-700 capitalize dark:text-white"
+        >
+          {{ tab.title }}
+        </h2>
+        <form @submit.prevent="submitForm(tab)">
           <div
-            class="relative overflow-hidden mt-5 max-w-4xl p-6 mx-auto bg-white border-2 border-e-gray-200 rounded-md dark:bg-gray-800"
+            v-for="(field, fieldIndex) in tab.fields"
+            :key="fieldIndex"
+            :class="[
+              'grid',
+              'grid-cols-1',
+              tab.gridLayout || 'sm:grid-cols-2',
+              field.gridClass,
+            ]"
           >
-            <div
-              v-for="(tab, index) in tabs"
-              :key="index"
-              v-show="activeTab === index"
+            <label
+              :for="field.id"
+              class="block text-gray-700 text-sm font-bold mb-2"
+            >{{ field.label }}</label
             >
-              <h2
-                class="text-2xl font-bold text-gray-700 capitalize dark:text-white"
+            <input
+              v-model="field.value"
+              :id="field.id"
+              :type="field.type"
+              :placeholder="field.placeholder"
+              class="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring"
+            />
+          </div>
+          <div class="grid grid-cols-1 sm:grid-cols-2">
+            <div class="grid grid-cols-1">
+              <label class="block text-gray-700 text-sm font-bold mb-2">Date</label>
+              <input
+                type="date"
+                class="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring"
+              />
+            </div>
+            <div class="grid grid-cols-1">
+              <label class="block text-gray-700 text-sm font-bold mb-2">Designation</label>
+              <select
+                v-model="designation"
+                class="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring"
               >
-                {{ tab.title }}
-              </h2>
-              <form @submit.prevent="submitForm(tab)">
-                <div
-                  v-for="(field, fieldIndex) in tab.fields"
-                  :key="fieldIndex"
-                  :class="[
-                    'grid',
-                    'grid-cols-1',
-                    tab.gridLayout || 'sm:grid-cols-2',
-                    field.gridClass,
-                  ]"
-                >
-                  <label
-                    :for="field.id"
-                    class="block text-gray-700 text-sm font-bold mb-2"
-                    >{{ field.label }}</label
-                  >
-                  <input
-                    v-model="field.value"
-                    :id="field.id"
-                    :type="field.type"
-                    :placeholder="field.placeholder"
-                    class="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring"
-                  />
-                </div>
-                <div class="flex items-center justify-between">
-                  <button
-                    type="submit"
-                    class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-                  >
-                    Submit
-                  </button>
-                </div>
-              </form>
+                <option value="HR">HR</option>
+                <option value="HOD">HOD</option>
+                <option value="IT DEVELOPER">IT DEVELOPER</option>
+              </select>
             </div>
           </div>
-        </div>
-      
-   
+          <div class="flex items-center justify-between">
+            <button
+              type="submit"
+              class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+            >
+              Submit
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -75,6 +93,7 @@ export default {
   data() {
     return {
       activeTab: 0,
+      designation: "",
       tabs: [
         {
           title: "Local Travelling",
