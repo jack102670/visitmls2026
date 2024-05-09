e5d3f943b1924cbe2f6bf003addf2af8a4d3f4d6
FatinRashid <fatinursyafawani@gmail.com>
2024-05-09 09:35:25 +0800

    update

--- src/views/e-claim/user-ui/FormTab.vue
+++ src/views/e-claim/user-ui/FormTab.vue
@@ -18,7 +18,7 @@
     </div>
 
     <div
-      class="relative overflow-hidden mt-5 max-w-4xl p-6  bg-white border-2 border-e-gray-200 rounded-md dark:bg-gray-800"
+      class="relative overflow-hidden mt-5 max-w-4xl p-6 bg-white border-2 border-e-gray-200 rounded-md dark:bg-gray-800"
     >
       <div
         v-for="(tab, index) in tabs"
@@ -28,42 +28,49 @@
         <h2 class="text-2xl font-bold text-gray-700 capitalize dark:text-white">
           {{ tab.title }}
         </h2>
-        <form @submit.prevent="submitForm(tab)">
-          <div
-            v-for="(field, fieldIndex) in tab.fields"
-            :key="fieldIndex"
-            :class="[
-              'grid',
-              'grid-cols-1',
-              tab.gridLayout || 'sm:grid-cols-2',
-              field.gridClass,
-            ]"
-          >
-            <label
-              :for="field.id"
-              class="block text-gray-700 text-sm font-bold mb-2"
-            >
-              {{ field.label }}
-              <span v-if="field.required" style="color: red;">*</span>
-            </label>
 
-            <input
-              v-model="field.value"
-              :id="field.id"
-              :type="field.type"
-              :placeholder="field.placeholder"
-              class="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring"
-            />
+        <div class="pt-4">
+          <hr class="" />
+
+          <div class="m-2">
+            <form @submit.prevent="submitForm(tab)">
+              <div
+                v-for="(field, fieldIndex) in tab.fields"
+                :key="fieldIndex"
+                :class="[
+                  'grid',
+                  'grid-cols-1',
+                  tab.gridLayout || 'sm:grid-cols-2',
+                  field.gridClass,
+                ]"
+              >
+                <label
+                  :for="field.id"
+                  class="block text-gray-700 text-sm font-bold mb-2"
+                >
+                  {{ field.label }}
+                  <span v-if="field.required" style="color: red">*</span>
+                </label>
+
+                <input
+                  v-model="field.value"
+                  :id="field.id"
+                  :type="field.type"
+                  :placeholder="field.placeholder"
+                  class="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring"
+                />
+              </div>
+              <div class="flex items-center justify-between">
+                <button
+                  type="save"
+                  class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+                >
+                  Save
+                </button>
+              </div>
+            </form>
           </div>
-          <div class="flex items-center justify-between">
-            <button
-              type="submit"
-              class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-            >
-              Submit
-            </button>
-          </div>
-        </form>
+        </div>
       </div>
     </div>
   </div>
@@ -74,7 +81,7 @@ export default {
   data() {
     return {
       activeTab: 0,
-      date : '',
+      date: "",
       tabs: [
         {
           title: "Local Travelling Form",
@@ -157,6 +164,7 @@ export default {
               id: "ForeignCurrencyAccommodation",
               label: "Foreign Currency",
               type: "number",
+              placeholder: "Accommodation",
               value: "",
               gridClass: "sm:col-span-2",
             },
@@ -178,6 +186,7 @@ export default {
               id: "ForeignCurrencyOthers",
               label: "Foreign Currency",
               type: "number",
+              placeholder: "Others",
               value: "",
               gridClass: "sm:col-span-2",
             },
@@ -463,7 +472,7 @@ export default {
       const formattedData = {};
 
       // Iterate through the fields of the current tab
-      tab.fields.forEach(field => {
+      tab.fields.forEach((field) => {
         // Use the field label as the key and the field value as the value
         formattedData[field.label] = field.value;
       });
@@ -478,7 +487,6 @@ export default {
       console.log("Formatted Form Data:", formattedData);
     },
   },
-
 };
 </script>
 
