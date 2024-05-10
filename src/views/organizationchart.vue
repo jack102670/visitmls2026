297d35528e107f14f42b56f8fd5ed968c170bd65
Hazman97 <105151296+Hazman97@users.noreply.github.com>
2024-05-10 17:28:01 +0800

    Refactor CSS classes for flex-wrap, border color, background color, and padding in output.css

--- src/views/organizationchart.vue
+++ src/views/organizationchart.vue
@@ -168,6 +168,19 @@
           </div>
           <div class="mb-4">
             <label for="nodeName" class="block text-gray-700 font-bold mb-2"
+              >Picture:</label
+            >
+            <div>
+              <input
+                type="file"
+                class="filepond"
+                ref="filepond"
+                accept="image/*"
+              />
+            </div>
+          </div>
+          <div class="mb-4">
+            <label for="nodeName" class="block text-gray-700 font-bold mb-2"
               >Node Name:</label
             >
             <input
