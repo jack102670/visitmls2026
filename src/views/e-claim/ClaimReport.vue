81db2267a7b2c89928a36534a72192bb1730d063
Hazman97 <105151296+Hazman97@users.noreply.github.com>
2024-05-23 10:52:18 +0800

    refactor: Remove unused button and update styling in ClaimReport.vue

--- src/views/e-claim/ClaimReport.vue
+++ src/views/e-claim/ClaimReport.vue
@@ -31,18 +31,7 @@
                 >
               </div>
             </button>
-            <button
-              @click="showContent"
-              class="w-30 p-1 rounded-lg items-center border-4 text-sm dark:bg-gray-900 dark:border-gray-700 bg-green-700 border text-white"
-            >
-              <div class="flex justify-center">
-                <span
-                  class="mr-2 ml-2 text-slate-100 hover:text-blue-200"
-                  @click="showtab = !showtab"
-                  >Add Expenses</span
-                >
-              </div>
-            </button>
+      
           </div>
         </div>
 
