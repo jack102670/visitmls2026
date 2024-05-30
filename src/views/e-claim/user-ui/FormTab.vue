dd66932f645dd84838ceea76706d4ab79604a014
Hazman97 <105151296+Hazman97@users.noreply.github.com>
2024-05-30 08:58:54 +0800

    chore: Refactor file upload logic in FormTab.vue and ClaimReport.vue

--- src/views/e-claim/user-ui/FormTab.vue
+++ src/views/e-claim/user-ui/FormTab.vue
@@ -185,7 +185,7 @@
             <div class="pt-4">
               <hr />
               <div class="m-2">
-                <form @submit.prevent="submitForm(subTab)">
+                <form @submit.prevent="submitForm2(subTab)">
                   <div
                     v-for="(field, fieldIndex) in subTab.fields"
                     :key="fieldIndex"
@@ -1291,6 +1291,7 @@ export default {
     submitForm(tab) {
       // Create an empty object to hold the formatted form data
       const formattedData = {};
+     
 
       
       // Iterate through the fields of the current tab
@@ -1320,6 +1321,38 @@ export default {
       //   console.log(`Form submitted for tab: ${tab.title}`, tab.fields);
       // }
     },
+    submitForm2() {
+  // Create an empty object to hold the formatted form data
+  const formattedData = {};
+
+  // Iterate over all tabs
+  this.entertainmentTabs.forEach((tab) => {
+    // Iterate through the fields of the current tab
+    tab.fields.forEach((field) => {
+      // Use the field label as the key and the field value as the value
+      formattedData[field.id] = field.value;
+    });
+
+    // If the tab is the "Attendees" tab, process the attendees
+    if (tab.title === "Attendees") {
+      const newAttendee = {};
+      tab.fields.forEach((field) => {
+        newAttendee[field.id] = field.value;
+        field.value = "";
+      });
+      tab.attendees.push(newAttendee);
+    }
+  });
+
+  // Add the tab title to the formatted data
+  formattedData["tabTitle"] = "Entertainment";
+
+  // Emit the formatted form data
+  this.$emit("formSubmitted", formattedData);
+
+  // Log the formatted form data to the console
+  console.log("Formatted Form Data:", formattedData);
+},
   },
 };
 </script>
