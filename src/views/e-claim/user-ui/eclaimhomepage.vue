22df9b20384d8531552dc0b717c80ee2560b398f
FatinRashid <fatinursyafawani@gmail.com>
2024-05-31 09:31:18 +0800

    .

--- src/views/e-claim/user-ui/eclaimhomepage.vue
+++ src/views/e-claim/user-ui/eclaimhomepage.vue
@@ -80,7 +80,7 @@
                           class="px-4 py-3.5 text-sm font-normal text-left rtl:text-right text-gray-500 dark:text-gray-400"
                         >
                           <div class="flex items-center gap-x-3">
-                            <span>Type Of Claim</span>
+                            <span>Type Of Request</span>
                           </div>
                         </th>
                         <th
@@ -88,7 +88,7 @@
                           class="px-4 py-3.5 text-sm font-normal text-left rtl:text-right text-gray-500 dark:text-gray-400"
                         >
                           <div class="flex items-center gap-x-3">
-                            <span>Claimant's Name</span>
+                            <span>Requester</span>
                           </div>
                         </th>
                         <th
