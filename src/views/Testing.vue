d1f467a77d6ad06a646c4125380fa7f06fef1162
Hazman97 <105151296+Hazman97@users.noreply.github.com>
2024-06-26 17:26:46 +0800

    chore: Update CSS classes and grid layout in AdminHRSummaryClaimpage.vue and AdminSummaryClaimpage.vue

--- src/views/Testing.vue
+++ src/views/Testing.vue
@@ -1,66 +1,246 @@
 <template>
-  <div class="relative inline-block w-1/2">
-    <div class="flex justify-between border border-gray-300 rounded overflow-hidden">
-      <input type="text" placeholder="Search.." v-model="search" @click="toggleDropdown" class="w-full py-2 pl-3 pr-2 focus:outline-none">
-      <div class="bg-gray-300 py-2 px-4 cursor-pointer" @click="toggleDropdown">
-        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" class="h-4 w-4 text-gray-600">
-          <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 9l-7 7-7-7"></path>
-        </svg>
+
+<button
+      class="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-700 transition duration-300"
+      @click="printPage"
+    >
+      Print
+    </button>
+  <div class="max-w-3xl mx-auto my-10 p-5 bg-white shadow-md rounded">
+    <header class="text-center mb-6">
+      <div class="flex justify-around">
+      <div>
+        <img
+          class="object-center hover:object-top  w-28 h-28 bg-white rounded-full mx-auto"
+          src="https://media.licdn.com/dms/image/D5603AQFfmIf3-IMSMw/profile-displayphoto-shrink_800_800/0/1683793088607?e=1724889600&v=beta&t=qRNeTeTBK1TxUplWP7eBhKkWAYYDSG4wY-YCkWjJyYw"
+          alt=""
+        />
+      </div>
+      <div>
+        <h1 class="text-2xl mt-5 uppercase font-bold">
+          muhammad hazman bin mohd adanan
+        </h1>
+        <p class="text-gray-600">Programmer</p>
+        <div class="mt-2">
+          <a
+            href="https://www.google.com/maps/search/?api=1&query=6.162127648192204,102.19026789345943"
+            class="text-blue-500 hover:underline ml-4"
+          >
+            Tumpat Kelantan </a
+          >|
+          <a
+            href="mailto:hazman5001@gmail.com"
+            class="text-blue-500 hover:underline"
+            >hazman5001@gmail.com</a
+          >
+          |
+          <a href="tel:+60169333092" class="text-blue-500 hover:underline">
+            0169333092
+          </a>
+          <div></div>
+        </div>
       </div>
     </div>
-    <div v-show="dropdownVisible" class="dropdown-content absolute bg-gray-100 min-w-full max-h-56 overflow-y-auto border border-gray-300 z-10 mt-2 rounded shadow-lg">
-      <a v-for="department in filteredDepartments" :key="department.department" @click="selectDepartment(department.department)" class="block text-black py-2 px-4 hover:bg-gray-200">
-        {{ department.department }}
-      </a>
-    </div>
+    </header>
+    <section class="mb-6">
+      <h2
+        class="text-center text-2xl font-bold border-b-2 border-gray-800 pb-1 mb-4"
+      >
+        Summary
+      </h2>
+      <p class="text-gray-700">
+        Experienced Front End Developer with Vue.js, Tailwind CSS. Skilled in
+        HTML, Python, C++, and C, with proficiency in tools like NetBeans and
+        Micropython. Strong background in networking and troubleshooting diverse
+        operating systems. with a passion for continuous learning and
+        contributing to both front-end development and IT support roles.
+        Experienced Software Developer with expertise in Vue.js, Tailwind CSS,
+        and modern web development technologies. Proven track record of
+        delivering high-quality applications on time and within budget.
+      </p>
+    </section>
+    <section class="mb-6">
+      <h2
+        class="text-2xl text-center font-bold border-b-2 border-gray-800 pb-1 mb-4"
+      >
+        Experience
+      </h2>
+      <div class="mb-4">
+        <h3 class="text-xl font-semibold">Frontend Developer</h3>
+        <div class="">
+          <p class="text-gray-600 flex justify-between">
+            <span>ABC Corp</span>
+            <span class="text-gray-600">Dec 2023 - Present</span>
+          </p>
+        </div>
+        <ul class="list-disc list-inside text-gray-700">
+          <li>Lead a team of developers to build scalable web applications.</li>
+          <li>
+            Implemented new features and optimized existing codebase for better
+            performance.
+          </li>
+          <li>
+            Collaborated with cross-functional teams to define project
+            requirements and deliverables.
+          </li>
+        </ul>
+      </div>
+      <div class="mb-4">
+        <h3 class="text-xl font-semibold">Software Developer</h3>
+        <p class="text-gray-600">XYZ Ltd | Jan 2017 - Dec 2019</p>
+        <ul class="list-disc list-inside text-gray-700">
+          <li>
+            Developed and maintained web applications using Vue.js and Tailwind
+            CSS.
+          </li>
+          <li>
+            Worked closely with designers to create user-friendly interfaces.
+          </li>
+          <li>
+            Assisted in the migration of legacy systems to modern web
+            technologies.
+          </li>
+        </ul>
+      </div>
+    </section>
+    <section class="mb-6">
+      <h2
+        class="text-2xl text-center font-bold border-b-2 border-gray-800 pb-1 mb-4"
+      >
+        Education
+      </h2>
+      <div class="mb-4">
+        <h3 class="text-xl font-semibold">
+          Bachelor of Computer Sciene And Maritime Informatic
+        </h3>
+        <p class="text-gray-600 justify-between flex">
+          <span> Universiti Malaysia Terengganu </span> <span>2019 - 2024</span>
+        </p>
+        <p class="text-gray-600">CGPA: 3.31</p>
+      </div>
+
+      <div class="mb-4">
+        <h3 class="text-xl font-semibold capatalize">
+          Diploma Electric & Electronic
+        </h3>
+        <p class="text-gray-600 justify-between flex">
+          <span> Politeknik Ibrahim Sultan </span> <span>2015 - 2018</span>
+        </p>
+        <p class="text-gray-600">HPNM: 2.87</p>
+      </div>
+    </section>
+    <section>
+      <h2
+        class="text-2xl font-bold border-b-2 text-center border-gray-800 pb-1 mb-4"
+      >
+        Skills
+      </h2>
+      <h2 class="text-lg capitalize font-semibold">soft skill:</h2>
+      <h2
+        class="text-2xl font-bold border-b-2 text-center border-slate-100 pb-1 mb-4"
+      ></h2>
+      <div class="mb-4 grid grid-cols-3 gap-4">
+        <ul class="list-disc list-inside">
+          <li>Vue.js</li>
+          <li>Tailwind CSS</li>
+          <li>JavaScript</li>
+          <li>HTML & CSS</li>
+          <li>Git & GitHub</li>
+        </ul>
+
+        <div>
+          <ul class="list-disc list-inside">
+            <li>SQL (Intermediate), MongoDB (Intermediate)</li>
+            <li>Git (Intermediate), Docker (Intermediate)</li>
+          </ul>
+        </div>
+        <div>
+          <ul class="list-disc list-inside">
+            <li>
+              HTML (Advanced), CSS (Advanced), Tailwind CSS (Intermediate)
+            </li>
+            <li>SQL (Intermediate), MongoDB (Intermediate)</li>
+            <li>Git (Intermediate), Docker (Intermediate)</li>
+          </ul>
+        </div>
+      </div>
+      <h2 class="text-lg capitalize font-semibold">technical Skill:</h2>
+
+      <h2 class="text-lg capitalize font-semibold">language:</h2>
+      <div class="mb-4 grid grid-cols-3 gap-4">
+        <ul class="list-disc list-inside">
+          <li>English (Basic)</li>
+        </ul>
+
+        <div>
+          <ul class="list-disc list-inside">
+            <li>Malay (Advanced)</li>
+          </ul>
+        </div>
+      </div>
+      <h2
+        class="text-lg font-bold border-b-2 text-center border-slate-100 pb-1 mb-4"
+      ></h2>
+      <h2 class="text-lg capitalize font-semibold">programming langueges</h2>
+      <div class="mb-4 grid grid-cols-3 gap-4">
+        <ul class="list-disc list-inside">
+          <li>Vue.js</li>
+          <li>Tailwind CSS</li>
+          <li>JavaScript</li>
+          <li>HTML & CSS</li>
+          <li>Git & GitHub</li>
+        </ul>
+
+        <div>
+          <ul class="list-disc list-inside">
+            <li>SQL (Intermediate), MongoDB (Intermediate)</li>
+            <li>Git (Intermediate), Docker (Intermediate)</li>
+          </ul>
+        </div>
+        <div>
+          <ul class="list-disc list-inside">
+            <li>
+              HTML (Advanced), CSS (Advanced), Tailwind CSS (Intermediate)
+            </li>
+            <li>SQL (Intermediate), MongoDB (Intermediate)</li>
+            <li>Git (Intermediate), Docker (Intermediate)</li>
+          </ul>
+        </div>
+      </div>
+    </section>
   </div>
 </template>
 
 <script>
-import axios from 'axios';
+
 
 export default {
-  name: 'DropdownS',
+  name: "DropdownS",
   data() {
-    return {
-      dropdownVisible: false,
-      search: '',
-      departments: []
-    };
+    return {};
   },
-  computed: {
-    filteredDepartments() {
-      return this.departments.filter(department => {
-        return department.department.toLowerCase().includes(this.search.toLowerCase())
-      });
-    }
-  },
+  computed: {},
   methods: {
-    toggleDropdown() {
-      this.dropdownVisible = !this.dropdownVisible;
+    printPage() {
+      window.print();
     },
-    selectDepartment(department) {
-      this.search = department;
-      this.dropdownVisible = false;
-    },
-    async fetchDepartments() {
-      try {
-        const response = await axios.get('http://172.28.28.91:97/api/User/GetDepartment');
-        this.departments = response.data.result;
-        console.log(this.departments);
-      } catch (error) {
-        console.error(`Error fetching departments: ${error}`);
-      }
-    }
   },
-  created() {
-    this.fetchDepartments();
-  }
 };
 </script>
 
 <style scoped>
-.dropdown-content {
-  max-height: 14rem; /* max-h-56 in Tailwind CSS */
+/* Add your regular styles here */
+
+@media print {
+  body {
+    width: 210mm;
+    height: 297mm;
+    margin: 0;
+    transform: scale(0.95); /* Adjust scale to fit the content within print margins */
+  }
+  .non-printable {
+    display: none; /* Hide elements not needed for print */
+  }
+  /* Add more print-specific styles as needed */
 }
 </style>
