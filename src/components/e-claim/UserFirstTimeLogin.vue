13276d0f2241119cb5e764fe0fd53982074aa073
FatinRashid <fatinursyafawani@gmail.com>
2024-07-03 17:54:29 +0800

    .

---
+++ src/components/e-claim/UserFirstTimeLogin.vue
@@ -0,0 +1,14 @@
+<template>
+  <div
+    class="bg-gray-500 bg-opacity-40 w-screen h-screen absolute left-0 top-0 z-50 flex justify-center items-center"
+  >
+    <div
+      class="popup overflow-y-auto lg:w-3/5 md:w-3/4 w-5/6 bg-white h-[90%] rounded-xl relative px-10 pb-6"
+    >
+      <!-- Heading Title -->
+      <h1 class="text-3xl font-bold py-6 border-b-2 border-black">
+        ACTIVATE USER ID
+      </h1>
+    </div>
+  </div>
+</template>
