4f3828ca61f24b3b26e254d9292c201ab8454588
Hazman97 <105151296+Hazman97@users.noreply.github.com>
2024-07-20 22:23:07 +0800

    chore: Remove commented code and clean up formatting in sidebar and e-claim components

--- src/components/Newsidebar.vue
+++ src/components/Newsidebar.vue
@@ -248,7 +248,7 @@
 
           <span class="mx-4 font-medium">Manage Vendor</span>
         </router-link>
-        <router-link
+        <!-- <router-link
           v-if="controlView === 'eclaim' && userDetails.userName.includes('FN')"
           class="flex items-center px-4 py-2 mt-5 text-slate-200 transition-colors duration-300 transform rounded-lg dark:text-gray-400 hover:bg-[#190a70] dark:hover:bg-gray-800 dark:hover:text-gray-200 hover:text-gray-400"
           :to="{ name: 'AdminDashboardpage' }"
@@ -269,7 +269,7 @@
           </svg>
 
           <span class="mx-4 font-medium">Dashboard Finance</span>
-        </router-link>
+        </router-link> -->
         <router-link
           v-if="controlView === 'eclaim' && userData.department === 'ICT'"
           class="flex items-center px-4 py-2 mt-5 text-slate-200 transition-colors duration-300 transform rounded-lg dark:text-gray-400 hover:bg-[#190a70] dark:hover:bg-gray-800 dark:hover:text-gray-200 hover:text-gray-400"
