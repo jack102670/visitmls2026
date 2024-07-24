e4fac5fa3db8aecd2f7b43c37769aa6cd86d3ffe
Hazman97 <105151296+Hazman97@users.noreply.github.com>
2024-07-24 08:58:13 +0800

    Update Newsidebar.vue to fix conditional rendering for eclaim department views

--- src/components/Newsidebar.vue
+++ src/components/Newsidebar.vue
@@ -271,7 +271,7 @@
           <span class="mx-4 font-medium">Dashboard Finance</span>
         </router-link> -->
         <router-link
-          v-if="controlView === 'eclaim' && userData.department === 'ICT'"
+          v-if="controlView === 'eclaim' && userData.department === 'FN'"
           class="flex items-center px-4 py-2 mt-5 text-slate-200 transition-colors duration-300 transform rounded-lg dark:text-gray-400 hover:bg-[#190a70] dark:hover:bg-gray-800 dark:hover:text-gray-200 hover:text-gray-400"
           :to="{ name: 'AdminDashboardpage' }"
         >
@@ -315,7 +315,7 @@
           <span class="mx-4 font-medium">Verify Claims</span>
         </router-link>
         <router-link
-          v-if="controlView === 'eclaim' && userDetails.userName.includes('HR')"
+          v-if="controlView === 'eclaim' && userData.department === ('HR')"
           class="flex items-center px-4 py-2 mt-5 text-slate-200 transition-colors duration-300 transform rounded-lg dark:text-gray-400 hover:bg-[#190a70] dark:hover:bg-gray-800 dark:hover:text-gray-200 hover:text-gray-400"
           :to="{ name: 'AdminHRDashboardpage' }"
         >
@@ -336,6 +336,28 @@
 
           <span class="mx-4 font-medium">Approve Claims</span>
         </router-link>
+        <!-- <router-link
+          v-if="controlView === 'eclaim' && userData.department === ('CB')"
+          class="flex items-center px-4 py-2 mt-5 text-slate-200 transition-colors duration-300 transform rounded-lg dark:text-gray-400 hover:bg-[#190a70] dark:hover:bg-gray-800 dark:hover:text-gray-200 hover:text-gray-400"
+          :to="{ name: 'AdminDashboardpage' }"
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke-width="1.5"
+            stroke="currentColor"
+            class="w-5 h-5"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="M7.5 14.25v2.25m3-4.5v4.5m3-6.75v6.75m3-9v9M6 20.25h12A2.25 2.25 0 0 0 20.25 18V6A2.25 2.25 0 0 0 18 3.75H6A2.25 2.25 0 0 0 3.75 6v12A2.25 2.25 0 0 0 6 20.25Z"
+            />
+          </svg>
+
+          <span class="mx-4 font-medium">Approve Claims</span>
+        </router-link> -->
 
         <a
           class="flex items-center px-4 py-2 mt-5 text-slate-200 transition-colors duration-300 transform rounded-lg dark:text-gray-400 hover:bg-[#190a70] dark:hover:bg-gray-800 dark:hover:text-gray-200 hover:text-gray-400"
