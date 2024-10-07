400846d76e5b383fd885c121b3b5cf0992e69f91
amir <amirabdulaziz01@gmail.com>
2024-10-07 09:59:22 +0800

    add plus icon

--- src/icons/fontawesome.js
+++ src/icons/fontawesome.js
@@ -2,8 +2,8 @@
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 
-import { faBars, faUser, faShoppingCart, faSignOutAlt, faChartLine, faGripHorizontal, faChevronRight, faSun, faAngleRight } from '@fortawesome/free-solid-svg-icons';
+import { faBars, faUser, faShoppingCart, faSignOutAlt, faChartLine, faGripHorizontal, faChevronRight, faSun, faAngleRight, faPlus } from '@fortawesome/free-solid-svg-icons';
 
-library.add(faBars, faUser, faSignOutAlt, faShoppingCart, faSignOutAlt, faChartLine, faGripHorizontal, faChevronRight, faSun, faAngleRight);
+library.add(faBars, faUser, faSignOutAlt, faShoppingCart, faSignOutAlt, faChartLine, faGripHorizontal, faChevronRight, faSun, faAngleRight, faPlus);
 
 export { FontAwesomeIcon };
