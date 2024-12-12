94f681a323c4ee70ba9aa7349c4a8d32fd5322c7
amir <amirabdulaziz01@gmail.com>
2024-12-12 12:36:06 +0800

    fix ui for verify request

--- src/views/e-forms/VerifyRequest/verifyRequest.vue
+++ src/views/e-forms/VerifyRequest/verifyRequest.vue
@@ -1,32 +1,32 @@
 <template>
     <div class="grid grid-cols-8 px-2 pb-4 pt-2 gap-2">
-        <div class="col-span-8 bg-white border-[1px] rounded-md p-4">
-            <div>
-                <h1 class="font-bold text-2xl text-primary">
-                    PKT GROUP COMPANIES
-                </h1>
-                <p class="text-sm text-primary font-semibold">Verify employee request</p>
+        <div class="col-span-8 md:mx-8 ">
+            <div class="py-2 ">
+                <breadcrumbs :breadcrumbs="breadcrumbs" />
+            </div>
+            <div class="space-y-2 bg-primary p-4  rounded-md">
+                <h1 class="font-bold text-md text-white uppercase"> Verify employee request</h1>
             </div>
         </div>
-        <div class="col-span-8 bg-white border-[1px] min-h-full rounded-md p-4">
+        <div class="col-span-8 bg-white border-[1px] min-h-full rounded-md p-4 md:mx-8">
             <VerifyTable />
         </div>
     </div>
 
 </template>
 <script>
-    import VerifyTable from "../../../components/EFormComponent/DashboardComponent/EFormTable.vue"
-    export default {
-        components: {
-            VerifyTable,
-        },
-        mounted() {
-            // console.log('Mounted hook triggered');
-            document.body.style.backgroundColor = '#F8FBFB';
-        },
-        beforeUnmount() { 
-            // console.log('Unmount hook triggered'); 
-            document.body.style.backgroundColor = '#CED1DA'; 
-        }
+import VerifyTable from "../../../components/EFormComponent/DashboardComponent/EFormTable.vue"
+export default {
+    components: {
+        VerifyTable,
+    },
+    mounted() {
+        // console.log('Mounted hook triggered');
+        document.body.style.backgroundColor = '#F8FBFB';
+    },
+    beforeUnmount() {
+        // console.log('Unmount hook triggered'); 
+        document.body.style.backgroundColor = '#CED1DA';
     }
+}
 </script>
