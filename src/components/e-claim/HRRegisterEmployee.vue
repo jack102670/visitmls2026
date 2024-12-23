a17a2ddac842091e03378ec232ed0073b30319ed
amir <amirabdulaziz01@gmail.com>
2024-12-23 15:57:27 +0800

    fix UI, Bugs in HRViewEMployee

--- src/components/e-claim/HRRegisterEmployee.vue
+++ src/components/e-claim/HRRegisterEmployee.vue
@@ -374,7 +374,7 @@ export default {
 
       axios
         .post(
-          'http://172.28.28.117:7165/api/Admin/Register_UserProfile',
+          'http://172.28.28.116:7165/api/Admin/Register_UserProfile',
           registerData
         )
         .then((response) => {
@@ -431,7 +431,7 @@ export default {
     },
     getAllPositions() {
       axios
-        .get('http://172.28.28.117:7239/api/User/GetDesignation')
+        .get('http://172.28.28.116:7239/api/User/GetDesignation')
         .then((response) => {
           this.AllPositions = response.data.result.map(
             (item) => item.designation
@@ -444,7 +444,7 @@ export default {
     },
     getAllCompanies() {
       axios
-        .get('http://172.28.28.117:7239/api/User/GetCompany')
+        .get('http://172.28.28.116:7239/api/User/GetCompany')
         .then((response) => {
           this.Company = response.data.result.map((item) => item.company_name);
         });
@@ -458,7 +458,7 @@ export default {
     async getEmpId(id) {
       try {
         const response = await axios.get(
-          `http://172.28.28.117:7239/api/User/GetEmployeeById/${id}`
+          `http://172.28.28.116:7239/api/User/GetEmployeeById/${id}`
         );
         return response.data.result[0].emp_id;
       } catch (error) {
@@ -537,7 +537,7 @@ export default {
 
       // Fetch additional data from the API and filter uniqueUsers
       axios
-        .get('http://172.28.28.117:7239/api/User/GetAllEmployees')
+        .get('http://172.28.28.116:7239/api/User/GetAllEmployees')
         .then((response) => {
           const existUserIds = response.data.result.map(
             (user) => user.username_id
@@ -574,7 +574,7 @@ export default {
       try {
         // Fetch additional data from the API
         const response = await axios.get(
-          'http://172.28.28.117:7239/api/User/GetAllEmployees'
+          'http://172.28.28.116:7239/api/User/GetAllEmployees'
         );
         const existUserIds = response.data.result.map(
           (user) => user.username_id
